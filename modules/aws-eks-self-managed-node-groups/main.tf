6026e42251b6b3b0e4a1d6b7448cfd330c0b2491
Constantine <constantine.vitt@gmail.com>
2022-10-17 16:11:25 -0400

    add desired capacity parameter for self managed node groups

--- modules/aws-eks-self-managed-node-groups/main.tf
+++ modules/aws-eks-self-managed-node-groups/main.tf
@@ -1,6 +1,7 @@
 resource "aws_autoscaling_group" "self_managed_ng" {
   name = "${var.context.eks_cluster_id}-${local.self_managed_node_group["node_group_name"]}"
 
+  desired_capacity    = local.self_managed_node_group["desired_capacity"]
   max_size            = local.self_managed_node_group["max_size"]
   min_size            = local.self_managed_node_group["min_size"]
   vpc_zone_identifier = length(local.self_managed_node_group["subnet_ids"]) == 0 ? (local.self_managed_node_group["subnet_type"] == "public" ? var.context.public_subnet_ids : var.context.private_subnet_ids) : local.self_managed_node_group["subnet_ids"]
